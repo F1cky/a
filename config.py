323b3f26b43068e40ba80da5ffd929fc05560bbd
F1cky <97504239+F1cky@users.noreply.github.com>
2022-02-17 19:00:38 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+token="5066903451:AAErw8Y58fpqUf5SNFDSGYGU1w6ISB_t6t0"
+dirbot="C:\\Users\\Administrator\\Desktop\\bot\\"
+otstuklink="https://t.me/+Kv5Ui1fEZTZiMzZi"
+manuallink="https://telegra.ph/Manual-po-traffiku-YT-12-02"
+chatlink="https://t.me/+KRLCK_UuIBtiZWFi"
+team="ALLIENS TEAM"
+minpass=0 
+mincookies=0
+amschatid="1392549702"
+rulestext = """
+1) Запрещено склеивать с файлом вредоносный софт.
+2) Запрещено сливать билд на VT(VirusTotal)/а так же ревёрсы. [Арб на 300$ + кик из тимы]
+3) Мы не несем ответственность за смерть дедиков.
+4) Мы имеем право отказать в заявке без причины.
+5) На вопрос "Почему не забираете запросы?" - ответ очевиден."""
